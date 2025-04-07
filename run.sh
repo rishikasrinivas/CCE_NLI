ab67f4417ce52a18be5a16933f047b28980e3e81
Your Name <you@example.com>
2025-04-07 01:57:14 +0000

    script to run from random to pruned

--- run.sh
+++ run.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-
-python3 code/lotteryTicket/snli_lottery_ticket_training.py --prune_metrics_dir models/snli/prune_metrics/lottery_ticket/bowman/Run2FIXEDLTH --model_type bowman 
-
-python3 code/activation.py --model_type bowman --save_activs_dir activations/bowman/lottery_ticket/Run2FIXEDLTH --prune_metrics_dir models/snli/prune_metrics/lottery_ticket/bowman/Run2FIXEDLTH
-
- python3 code/pruning_explanation.py --expls_mask_root_dir exp/bowman/lottery_ticket/Run2FIXEDLTH --prune_metrics_dir models/snli/prune_metrics/lottery_ticket/bowman/Run2FIXEDLTH --model_type bowman --pruning_method lottery_ticket --ckpt models/snli/prune_metrics/lottery_ticket/bowman/Run2FIXEDLTH/0_Pruning_Iter/model_best.pth --activations_root_dir activations/bowman/lottery_ticket/Run2FIXEDLTH
+python -c "import os; print(os.path.abspath('analyze.py'))"
+python3 code/analyze.py --model_type bert --filename Run3
 
 
-python3 code/wanda/snli_wanda_training.py --prune_metrics_dir models/snli/prune_metrics/wanda/bowman/Run2FIXEDLTH --model_type bowman --ckpt models/snli/Run2_bowman_random_inits.pth
+python3 code/lotteryTicket/snli_lottery_ticket_training.py --prune_metrics_dir BERT/models/lottery_ticket/Run3 --model_type bert --ckpt BERT/models/Random/Run3/bert_random_inits.pth
 
-python3 code/activation.py --model_type bowman --save_activs_dir activations/bowman/wanda/Run2FIXEDLTH --prune_metrics_dir models/snli/prune_metrics/wanda/bowman/Run2FIXEDLTH
+python3 code/activation.py --model_type bert --save_activs_dir BERT/activations/lottery_ticket/Run3 --prune_metrics_dir BERT/models/lottery_ticket/Run3
 
-python3 code/pruning_explanation.py --expls_mask_root_dir exp/bowman/wanda/Run2FIXEDLTH --prune_metrics_dir models/snli/prune_metrics/wanda/bowman/Run2FIXEDLTH --model_type bowman --pruning_method wanda --ckpt models/snli/prune_metrics/wanda/bowman/Run2FIXEDLTH/0_Pruning_Iter/model_best.pth --activations_root_dir activations/bowman/wanda/Run2FIXEDLTH
+python3 code/pruning_explanation.py --model_type bert --pruning_method lottery_ticket --ckpt BERT/models/Run3/lottery_ticket/0_Pruning_Iter/model_best.pth --filename Run3
+
+
+python3 code/wanda/snli_wanda_training.py --prune_metrics_dir BERT/models/wanda/Run3 --model_type bert --ckpt BERT/models/Run3/lottery_ticket/0_Pruning_Iter/model_best.pth
+
+python3 code/activation.py --model_type bert --save_activs_dir BERT/activations/wanda/Run3 --prune_metrics_dir BERT/models/wanda/Run3/0_Pruning_Iter/model_best.pth
+
+python3 code/pruning_explanation.py --model_type bert --pruning_method wanda --ckpt BERT/models/Run3/wanda/0_Pruning_Iter/model_best.pth --filename Run3
  
-python3 code/pruning_explanation.py --expls_mask_root_dir exp/bert/osscar/Run2FIXEDLTH --prune_metrics_dir models/snli/prune_metrics/osscar/bert/Run2FIXEDLTH --model_type bert --pruning_method osscar --ckpt models/snli/prune_metrics/osscar/bert/Run2FIXEDLTH/0_Pruning_Iter/model_best.pth --activations_root_dir activations/bert/osscar/Run2FIXEDLTH
- 
+
 
