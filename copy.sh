9b0cffae262bab1103ba0ba21bce4f09d74a96a8
rishikasrinivas <118067369+rishikasrinivas@users.noreply.github.com>
2026-01-23 13:46:06 -0800

    Delete copy.sh

--- copy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Execute the find and copy command
-find "CCE_NLI_real" -type f -not -path '*/\.*' -not -path '*/__*/*' -exec cp --parents {} /tutorial \;
-
-echo "Files copied successfully from '$fldr' to /tutorial"
